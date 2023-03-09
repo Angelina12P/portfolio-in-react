2d1fc21df747aa46651dbc98dc9a1538436835cf
Angel <rachel@Rachels-MacBook-Air.local>
2023-03-09 13:16:11 +0000

    set up app.js

--- portfolio-in-react/src/App.js
+++ portfolio-in-react/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./components/HomePage";
+import Projects from "./components/ProjectPage";
+import Contact from "./components/ContactPage";
+import Header from "./components/Header";
+// import Project from "./components/Project";
+import {Switch, Route} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contact" component={Contact} />
+      </Switch>
     </div>
   );
 }
