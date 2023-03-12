e7ed5d3da6f5bf15b0c8e2732e0a075d4000007f
Angel <rachel@Rachels-MacBook-Air.local>
2023-03-12 13:55:43 +0000

    projects showing on page. (In v ugly colours so i can see what's what)

--- components/Project.js
+++ components/Project.js
@@ -1,16 +1,35 @@
 import React from "react";
 
-function Project(){
+function Project(props){
 
+    return (
+        <div className="card">
+          <div className="img-container">
+            <img alt={props.name} src={props.image} />
+          </div>
+          <div className="content">
+            <ul>
+              <li>
+                <strong>Project title</strong> {props.title}
+              </li>
+              <li>
+                <strong>Link to the deployed version</strong> {props.link}
+              </li>
+              <li>
+                <strong>Link to the GitHub repository</strong> {props.repoLink}
+              </li>
+              <li>
+                <strong>GIF or screenshot of the deployed application</strong> {props.image}
+              </li>
+            </ul>
+          </div>
+        </div>
+      );
 
     
     // * Must be a reusable component that ingests JSON data as props and renders a single instance for each project
     // * Must utilize Router props to properly render the right project based on user selection
-    // * Must render the following info:
-    // * Project title
-    // * Link to the deployed version
-    // * Link to the GitHub repository
-    // * GIF or screenshot of the deployed application
+
 }
 
 
--- components/ProjectsPage.js
+++ components/ProjectsPage.js
@@ -1,17 +1,56 @@
 import React from "react";
-import Project from "./Project";
+import Project from "./Project.js";
+import projects from "./projects.json";
 // import ProjectsPageStyle from "./ProjectsPageStyle.css"
+import Wrapper from "./Wrapper";
+import Title from "./Title";
 
 
+function Projects() {
 
-function Projects(){
-    // get projects 
-    return(
-        <div>
-            <h1>Projects:</h1>
-            {/* insert projects x6  */}
-        </div>
-    )
+    return (
+        <Wrapper>
+            <Title> Projects: </Title>
+            <Project
+                title={projects[0].title}
+                link={projects[0].link}
+                repoLink={projects[0].repoLink}
+                image={projects[0].image}
+            />
+            <Project
+                title={projects[1].title}
+                link={projects[1].link}
+                repoLink={projects[1].repoLink}
+                image={projects[1].image}
+            />
+            <Project
+                title={projects[2].title}
+                link={projects[2].link}
+                repoLink={projects[2].repoLink}
+                image={projects[2].image}
+            />
+            <Project
+                title={projects[3].title}
+                link={projects[3].link}
+                repoLink={projects[3].repoLink}
+                image={projects[3].image}
+            />
+            <Project
+                title={projects[4].title}
+                link={projects[4].link}
+                repoLink={projects[4].repoLink}
+                image={projects[4].image}
+            />
+            <Project
+                title={projects[5].title}
+                link={projects[5].link}
+                repoLink={projects[5].repoLink}
+                image={projects[5].image}
+            />
+
+        </Wrapper>
+    );
+
 
 }
 
