3b025482a2948df7e08df9f492255086d9b4cc62
Angel <rachel@Rachels-MacBook-Air.local>
2023-03-12 09:42:05 +0000

    folder restructuring

---
+++ components/ContactPage.js
@@ -0,0 +1,30 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ContactPageStyle from "./ContactPageStyle.css";
+
+
+function Contact(){
+    return(
+        <div>
+            <h1> Contact:</h1>
+            <h2>angelwork128@outlook.com</h2>
+            <h2> Send me a message:</h2>
+            <form>
+                {/* <input type="text">Name:</input>
+                <input type="email">Email</input>
+                <input type="text">Message</input> */}
+                <button>Send</button>
+            </form>
+            <h2>Or find me here:</h2>
+            <h3>github</h3>
+            <h3>linkdin</h3>
+            <div>
+                <h2>Download my CV here:</h2>
+                <h3>PDF</h3>
+            </div>
+        </div>
+    );
+}
+
+
+export default Contact
